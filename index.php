e4d2ce3c7c5c64bbe707080eeb96ef21766ed496
vitaliyzhiv <zhmud12@gmail.com>
2024-11-08 22:50:20 +0200

    add dynamic to index page

--- index.php
+++ index.php
@@ -1,58 +1,58 @@
-<?php 
+<?php
 // Починаємо сесію
 
-include 'path.php'; 
+include 'path.php';
 include  BASE_PATH . '/app/controllers/users.php';
-include BASE_PATH . '/app/include/header.php'; 
+include BASE_PATH . '/app/include/header.php';
 
 $topics = selectAll('topics');
+$posts = selectAll('posts');
+$users = selectAll('users');
+$postsAdm = selectAllFromPostsWithUsers('posts', 'users');
+
+$selectPostsSlider = selectAll('posts', [
+    'status' => 1,
+    'topic_id' => 16
+]);
+
 
 ?>
 
-    <!-- блок карусели -->
-    <div class="container">
-        <div class="row">
-            <h2 class="slider-title p-3 text-center">Топ посты</h2>
-        </div>
-        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
-            <div class="carousel-inner">
-                <?php
-                // создаем массив с id  изображений
-                $images = [87, 88, 89, 90, 91];
-                $slides = count($images); // Количество слайдов
-
-                $slide_links = [
-                    'article1.php',
-                    'article2.php',
-                    'article3.php',
-                    'article4.php',
-                    'article5.php',
-                ];
-
-                for ($i = 0; $i < $slides; $i++):
-                    // Добавляем класс active к первому слайду.
-                    $activeClass = ($i === 0) ? 'active' : '';
-                    $imageSrc = "https://picsum.photos/id/{$images[$i]}/800/400";
-                ?>
-                    <div class="carousel-item <?= $activeClass ?>">
-                        <img src="<?= $imageSrc ?>" class="d-block w-100" alt="Slide <?= $i + 1 ?>">
-                        <div class="carousel-caption d-none d-md-block">
-                            <h5><a href="<?= $slide_links[$i] ?>">Slide <?= $i + 1 ?> label</a></h5>
-                        </div>
-                    </div>
-                <?php endfor; ?>
-
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
-        </div>
+<!-- блок карусели -->
+<div class="container">
+    <div class="row">
+        <h2 class="slider-title p-3 text-center">Топ посты</h2>
     </div>
+    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
+        <div class="carousel-inner">
+            <?php for ($i = 0; $i < count($selectPostsSlider); $i++):
+                $slider = $selectPostsSlider[$i];
+                $activeClass = ($i === 0) ? 'active' : '';
+                $img = !(empty($slider['img'])) ? $slider['img'] : 'default_image.jpg';
+            ?>
+                <div class="carousel-item <?= $activeClass ?>">
+                    <img src="<?= IMAGES_URL . 'posts/' . $img; ?>" class="d-block w-100" alt="<?= $slider['title']; ?>">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>
+                            <a href="<?php echo BASE_URL . 'single.php?post=' . $slider['id']; ?>">
+                                <?= $slider['title']; ?>
+                            </a>
+                        </h5>
+                    </div> <!-- carousel-caption -->
+                </div> <!-- carousel-item -->
+            <?php endfor; ?>
+        </div> <!--  carousel-inner -->
+
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div> <!-- Закриваючий тег для carousel -->
+
     <!-- блок карусели конец -->
 
     <!-- Блок main -->
@@ -61,38 +61,41 @@ $topics = selectAll('topics');
             <!-- Main content -->
             <div class="main-content col-md-9 col-12">
                 <h2>Последние публикации</h2>
-                <div class="post row">
-                    <div class="img col-12 col-md-4">
-                        <img src="https://picsum.photos/id/237/200" alt="" class="img-thumbnail">
-                    </div>
-                    <div class="post_text col-12 col-md-8">
-                        <h3>
-                            <a href="#">Прикольная статья на тему динамического сайта</a>
-                        </h3>
-                        <i class="far fa-user"></i> <span>Имя автора</span>
-                        <i class="far fa-calendar"></i> <span>July 12 2006</span>
+                <!-- Проходимся циклом по всем постам в массиве postsAdm  -->
+                <?php foreach ($postsAdm as $post): ?>
+                    <!-- Выводим пост  если его статус равен 1 -->
+                    <?php if ($post['status'] == 1): ?>
+                        <div class="post row">
+                            <div class="img col-12 col-md-4">
+                                <?php $img = !(empty($post['img'])) ? $post['img'] : 'default_image.jpg'; ?>
+                                <img src="<?php echo IMAGES_URL . 'posts/' . $img;  ?> " alt="<?php echo $post['title']; ?>" class="img-thumbnail">
+                            </div>
+                            <div class="post_text col-12 col-md-8">
+                                <h3>
+                                    <!-- Укорачиваем заголовок если он больше 120 символов -->
+                                    <?php if (strlen($post['title']) > 120): ?>
+                                        <a href="<?php echo BASE_URL . 'single.php?id=' . $post['id']; ?>">
+                                            <?php echo substr($post['title'], 0, 120) . '...'; ?>
+                                        </a>
+                                    <?php else: ?>
+                                        <a href="<?php echo BASE_URL . 'single.php?post=' . $post['id']; ?>">
+                                            <?php echo $post['title']; ?>
+                                        </a>
+                                    <?php endif; ?>
 
-                        <p class="preview-text">
-                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi cupiditate architecto aliquam recusandae eius, hic rem beatae atque. Enim minima pariatur molestiae saepe officiis reprehenderit molestias veritatis sed beatae aliquid.
-                        </p>
-                    </div>
-                </div>
-                <div class="post row">
-                    <div class="img col-12 col-md-4">
-                        <img src="https://picsum.photos/id/421/200" alt="" class="img-thumbnail">
-                    </div>
-                    <div class="post_text col-12 col-md-8">
-                        <h3>
-                            <a href="#">Прикольная статья на тему динамического сайта</a>
-                        </h3>
-                        <i class="far fa-user"></i> <span>Имя автора</span>
-                        <i class="far fa-calendar"></i> <span>July 12 2006</span>
-
-                        <p class="preview-text">
-                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi cupiditate architecto aliquam recusandae eius, hic rem beatae atque. Enim minima pariatur molestiae saepe officiis reprehenderit molestias veritatis sed beatae aliquid.
-                        </p>
-                    </div>
-                </div>
+                                </h3>
+                                <i class="far fa-user"></i> <span><?php echo $post['username']; ?></span>
+                                <?php $date = explode(' ', $post['created_date'])[0]; ?>
+                                <i class="far fa-calendar"></i> <span><?php echo $date; ?></span>
+                                <?php if (mb_strlen($post['content'], 'UTF-8') > 200): ?>
+                                    <?php echo mb_substr($post['content'], 0, 200, 'UTF-8') . '...'; ?>
+                                <?php else: ?>
+                                    <?php echo $post['content']; ?>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+                    <?php endif; ?>
+                <?php endforeach; ?>
             </div>
             <!-- Main content end -->
 
@@ -109,7 +112,7 @@ $topics = selectAll('topics');
                 <div class="section topics">
                     <h3 style="color: #006969;">Категории</h3>
                     <ul>
-                    <?php include BASE_PATH . '/app/include/sidebar-main.php';  ?>
+                        <?php include BASE_PATH . '/app/include/sidebar-main.php';  ?>
                     </ul>
                 </div>
 
@@ -121,4 +124,4 @@ $topics = selectAll('topics');
 
     <!-- Блок main конец -->
 
-<?php include BASE_PATH . '/app/include/footer.php'; ?>
+    <?php include BASE_PATH . '/app/include/footer.php'; ?>
