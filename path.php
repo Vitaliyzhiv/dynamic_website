eb202a805404324c868bf20b22c21d8a6f1eaf46
vitaliyzhiv <zhmud12@gmail.com>
2024-11-08 22:50:49 +0200

    add const for images dir

--- path.php
+++ path.php
@@ -9,3 +9,4 @@ define('BASE_URL', 'http://localhost/dynamic_website/');
 // независимо от того, где находится скрипт на сервере.
 define('ROOT_PATH', realpath(dirname(__FILE__)));
 
+define('IMAGES_URL', value: 'http://localhost/dynamic_website/assets/images/');
