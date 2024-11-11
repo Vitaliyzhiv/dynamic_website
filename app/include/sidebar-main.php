d5bb11257f4d2a3fba479e44fdccdad6abe546fc
vitaliyzhiv <zhmud12@gmail.com>
2024-11-11 02:43:35 +0200

    updated paths in sidebar main

--- app/include/sidebar-main.php
+++ app/include/sidebar-main.php
@@ -1,3 +1,3 @@
 <?php foreach ($topics as $topic): ?>
-    <li><a href="<?= BASE_URL . "/admin/topics/edit.php?id=" . $topic['id'] ?>"><?= $topic['name']; ?></a></li>
+    <li><a href="<?= BASE_URL . "category.php?topic_id=" . $topic['id'] ?>"><?= $topic['name']; ?></a></li>
 <?php endforeach; ?>
