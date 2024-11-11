3256653856a5170e81a0d9fbdb0119d6d768bf44
vitaliyzhiv <zhmud12@gmail.com>
2024-11-11 02:42:37 +0200

    created single.php for specified post

--- single.php
+++ single.php
@@ -1,25 +1,42 @@
-<?php include 'app/include/header.php'; ?>
+<?php
+
+include 'path.php';
+include  BASE_PATH . '/app/database/db.php';
+include BASE_PATH . '/app/include/header.php';
+
+$post = selectOne('posts', ['id' => $_GET['post']]);
+// tt($post);
+$topics = selectAll('topics');
+
+?>
 
 <!-- Блок main -->
 <div class="container">
     <div class="content row">
         <!-- Main content -->
         <div class="main-content col-md-9 col-12">
-            <h2>Заголовок какой-то статьи нужно несколько строк написать чтобы увидеть как будет выглядеть статья </h2>
+            <h2 class="single_post_title">
+                <!-- Укорачиваем заголовок если он больше 120 символов -->
+                <a href="<?php echo BASE_URL . 'single.php?post=' . $post['id']; ?>">
+                    <?php echo $post['title']; ?>
+                </a>
+            </h2>
             <div class="single_post row">
-                <div class="img col-12 ">
-                    <img src="https://picsum.photos/id/237/800/300" alt="" class="img-thumbnail">
-
+                <div class="img col-12">
+                    <?php $img = !(empty($post['img'])) ? $post['img'] : 'default_image.jpg'; ?>
+                    <img src="<?php echo IMAGES_URL . 'posts/' . $img;  ?> " alt="<?php echo $post['title']; ?>" class="img-fluid">
                 </div>
-
+                <?php $author = selectOne('users', ['id' => $post['id_user']]);
+                $author = $author['username'];
+                ?>
                 <div class="info">
-                    <i class="far fa-user"></i> <span>Имя автора</span>
-                    <i class="far fa-calendar"></i> <span>July 12 2006</span>
+                    <i class="far fa-user"></i> <span><?php echo $author; ?></span>
+                    <?php $date = explode(' ', $post['created_date'])[0]; ?>
+                    <i class="far fa-calendar"></i> <span><?php echo $date; ?></span>
                 </div>
 
                 <div class="single_post_text col-12 ">
-                    <!-- много текста ниже -->
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem, vel reprehenderit fugiat debitis labore <a href="#">veniam</a> veniam, magnam nisi, labore corrupti reprehenderit autem. Autem, repudiandae? Sunt, eveniet commodi! Dignissimos, quaerat veniam. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem, vel reprehenderit fugiat debitis labore veniam, magnam nisi, labore corrupti reprehenderit autem. Autem, repudiandae? Sunt, eveniet commodi! Dignissimos, quaerat veniam.
+                    <?php echo $post['content']; ?>
                 </div>
             </div>
         </div>
@@ -38,13 +55,7 @@
             <div class="section topics">
                 <h3>Категории</h3>
                 <ul>
-                    <li><a href="#">Программирование</a></li>
-                    <li><a href="#">Дизайн</a></li>
-                    <li><a href="#">Маркетинг</a></li>
-                    <li><a href="#">Бизнес</a></li>
-                    <li><a href="#">Технологии</a></li>
-                    <li><a href="#">Другое</a></li>
-
+                    <?php include BASE_PATH . '/app/include/sidebar-main.php';  ?>
                 </ul>
             </div>
 
